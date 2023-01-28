427cfc7f1422ca18db60a1f0b12e54e5fce145a2
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-28 20:36:12 -0300

    Aprimorando a aplicação - TypeScript no React
    Agora que já vimos o que é o TypeScript, vamos migrar o nosso projeto para utilizar TypeScript.
    Vamos fazer isso criando um novo projeto Vite com a configuração react-ts, que já configura o TypeScript automaticamente pra gente.
    Vamos também começar a migrar nossos componentes já criados para TypeScript.

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Post } from './components/Post';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/rodrigo-aguiar.png',
+      name: 'Nome do Usuário',
+      role: 'Cargo do usuário'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-12-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/rodrigo-aguiar.png',
+      name: 'Nome do Usuário 2',
+      role: 'Cargo do usuário 2'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-12-10 20:00:00'),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map(post => {
+            return (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          );
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { PencilLine } from 'phosphor-react';
+
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1674240568812-d7481f3699a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/rodrigo-aguiar.png" />
+
+        <strong>Nome do Usuario</strong>
+        <span>Cargo do usuario</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
